5c6595beaaf11156db39bd28fe29668bb0759a8f
johngiannini <john.giannini@cognaize.com>
2024-12-09 14:47:25 -0600

    Anotate solution

--- functions/lets_do_this_II.py
+++ functions/lets_do_this_II.py
@@ -13,13 +13,16 @@ def lets_do_this(sc):
 
     Output().write(message="Here's the multi-threaded part:", output_type=OutputType.Log)
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        # Here futures is a dictionary, rather than a list as we'd been using previously
         futures = {executor.submit(_do_this, i, output_queue): i for i in range(5)}
 
         while futures:
-            while not output_queue.empty():
+            while not output_queue.empty():  # Each time we loop, watch the Queue for output and print anything there
                 Output().write(message=output_queue.get(), output_type=OutputType.Log)
 
+            # Check for completed futures, but don't wait for them too long (0.1 seconds)
             done, _ = concurrent.futures.wait(futures, timeout=0.1, return_when=concurrent.futures.FIRST_COMPLETED)
+            # Process any futures that are completed and remove them from futures (or the outer while loop will never end)
             for future in done:
                 task_id = futures[future]
                 result = future.result()  # Retrieve the result
