a454136977b77cb7588090c7e8ea6de336cacf90
johngiannini <john.giannini@cognaize.com>
2024-12-09 11:36:22 -0600

    Add error reproduction

--- functions/lets_do_this.py
+++ functions/lets_do_this.py
@@ -1,8 +1,23 @@
+import concurrent.futures
+from loguru import logger
+
 from output.output import Output
 from output.enums import OutputType
 
 
 def lets_do_this():
     Output().write(message="Here we go!", output_type=OutputType.Log)
+    outputter = Output()
+    Output().write(message="Copied the Output() instance...", output_type=OutputType.Log)
+    Output().write(message="Here's the multi-threaded part:", output_type=OutputType.Log)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(_do_this, i, outputter) for i in range(2)]
+    for future in futures:
+        future.result()
     Output().write(message="And there we go!", output_type=OutputType.Log)
     Output().write(message="Test Completed.", output_type=OutputType.Complete)
+
+
+def _do_this(i, outputter):
+    logger.info(f"Doing this {i}...")
+    outputter.write(message=f"Doing this {i}...", output_type=OutputType.Log)
